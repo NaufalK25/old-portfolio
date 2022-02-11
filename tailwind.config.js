62b1313075d79994709eea82b40825ebdd7ddee9
NaufalK25 <naufalkateni2001@gmail.com>
2022-02-11 16:44:51 +0700

    Uniqnore eslintrc, tailwindconfig, and tsconfig

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{ejs,html}", "./dist/**/js/*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Roboto", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
