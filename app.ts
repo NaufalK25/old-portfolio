e7d7b4a2fd3d8493fe94e84b96b1ebacde167af1
NaufalK25 <naufalkateni2001@gmail.com>
2022-02-16 21:07:50 +0700

    8th

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import expressLayouts from 'express-ejs-layouts';
-import morgan from 'morgan';
 import { baseUrl, port } from './src/config/constants';
 import { baseRoutes } from './src/routes/baseRoutes';
 
@@ -11,7 +10,6 @@ dotenv.config();
 const app = express();
 
 // Third Party Middlewares
-app.use(morgan('dev'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
