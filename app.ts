6ef65b343084f532257cbd6526ef1ee7f6203fad
NaufalK25 <naufalkateni2001@gmail.com>
2022-08-06 19:24:04 +0700

    add morgan

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import expressLayouts from 'express-ejs-layouts';
+import morgan from 'morgan';
 import { baseUrl, port } from './src/config/constants';
 import router from './src/routes/index.route';
 
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 
 // Third Party Middlewares
+app.use(morgan('dev'));
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
